fc1841a859f550c2cc5c029e83d1e48a5fbadb1a
Sebastian Sarria <161655160+JSebasSN@users.noreply.github.com>
2024-05-07 10:49:31 +0000

    nuevoFunc

---
+++ login.js
@@ -0,0 +1,5 @@
+document.getElementById('login').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    window.location.href ="page.html";
+});
--- script.js
+++ script.js
@@ -1,59 +1,75 @@
-document.getElementById('login').addEventListener('submit', function(event){
-    event.preventDefault();
+var searchWords = ['ENTREGADO','INCIDENCIA','DISPONIBLE EN PUDO','DEPOSITADO EN PUNTO','EN REPARTO A DESTINATÁRIO'];
 
-    window.location.href ="page.html";
-});
-
-function contarPalabraEnFila(csvData, fila, palabra) {
-    // Dividir los datos CSV en filas
-    const filas = csvData.split('\n');
+document.getElementById('excel-file').addEventListener('change', function() {
+    var fileInput = document.getElementById('excel-file');
+    var file = fileInput.files[0];
+    var reader = new FileReader();
     
-    // Obtener la fila específica
-    const filaEspecifica = filas[fila - 1];
-
-    // Contador para la palabra específica
-    let contador = 0;
-
-    // Dividir la fila en palabras
-    const palabras = filaEspecifica.split(',');
-
-    // Iterar sobre las palabras y contar las ocurrencias
-    palabras.forEach((palabraActual) => {
-        if (palabraActual.trim() === palabra) {
-            contador++;
+    reader.onload = function(event) {
+        var data = new Uint8Array(event.target.result);
+        var workbook = XLSX.read(data, { type: 'array' });
+        var sheetName = workbook.SheetNames[0]; // Suponiendo que estamos leyendo la primera hoja del libro de trabajo
+        var worksheet = workbook.Sheets[sheetName];
+        var range = XLSX.utils.decode_range(worksheet['!ref']);
+        var totalWordCount = 0; // Inicializamos el contador de la palabra total
+        for (var R = range.s.r; R <= range.e.r; ++R) { // Recorremos todas las filas
+            for (var C = range.s.c; C <= range.e.c; ++C) { // Recorremos todas las columnas
+                var address = { c: C, r: R };
+                var cellRef = XLSX.utils.encode_cell(address);
+                var cell = worksheet[cellRef];
+                if (cell && cell.t === 's') {
+                    var cellValue = cell.v.toLowerCase();
+                    searchWords.forEach(function(searchWord) {
+                        if (cellValue === searchWord.toLowerCase()) {
+                            totalWordCount++; // Incrementamos el contador si encontramos la palabra
+                        }
+                    });
+                }
+            }
         }
-    });
-
-    return contador;
-}
-
-function handleFileSelect(event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-        const csvData = e.target.result;
-        const palabra = 'ENTREGADO,EN REPARTO A DESTINATÁRIO,INCIDENCIA,DEPOSITADO EN PUNTO';
-        const fila = 10; // Fila específica
-
-        const cantidad = contarPalabraEnFila(csvData, fila, palabra);
-        // Mostrar el resultado en el elemento div
-        console.log(csvData);
-        document.getElementById('resultado').innerText = cantidad;
+        var resultDiv = document.getElementById('resultEnv');
+        resultDiv.innerHTML = totalWordCount;
     };
-
-    reader.readAsText(file);
-}
-
-// Obtener referencia al botón y asignarle el evento de clic
-document.getElementById('loadFileBtn').addEventListener('click', function () {
-    // Simular clic en el input de archivo
-    document.getElementById('fileInput').click();
+    
+    reader.readAsArrayBuffer(file);
 });
 
-// Asignar el evento de cambio de archivo al input de tipo archivo
-// Asignar el evento de cambio de archivo al input de tipo archivo
-document.getElementById('loadFileBtn').addEventListener('change', handleFileSelect, false);
+var searchWordsReco = ['REALIZADO','ENVIO GENERADO','REALIZADA'];
+
+document.getElementById('excel-file').addEventListener('change', function() {
+    var fileInput = document.getElementById('excel-file');
+    var file = fileInput.files[0];
+    var reader = new FileReader();
+    
+    reader.onload = function(event) {
+        var data = new Uint8Array(event.target.result);
+        var workbook = XLSX.read(data, { type: 'array' });
+        var sheetName = workbook.SheetNames[0]; // Suponiendo que estamos leyendo la primera hoja del libro de trabajo
+        var worksheet = workbook.Sheets[sheetName];
+        var range = XLSX.utils.decode_range(worksheet['!ref']);
+        var totalWordCount = 0; // Inicializamos el contador de la palabra total
+        for (var R = range.s.r; R <= range.e.r; ++R) { // Recorremos todas las filas
+            for (var C = range.s.c; C <= range.e.c; ++C) { // Recorremos todas las columnas
+                var address = { c: C, r: R };
+                var cellRef = XLSX.utils.encode_cell(address);
+                var cell = worksheet[cellRef];
+                if (cell && cell.t === 's') {
+                    var cellValue = cell.v.toLowerCase();
+                    searchWordsReco.forEach(function(searchWord) {
+                        if (cellValue === searchWord.toLowerCase()) {
+                            totalWordCount++; // Incrementamos el contador si encontramos la palabra
+                        }
+                    });
+                }
+            }
+        }
+        var resultDiv = document.getElementById('resultReco');
+        resultDiv.innerHTML = totalWordCount;
+    };
+    
+    reader.readAsArrayBuffer(file);
+});
+
 
 
 
